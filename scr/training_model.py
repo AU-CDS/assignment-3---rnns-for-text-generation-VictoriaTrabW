84ec90b87ac9710512fbb4316b81c4d18c50e597
victoriaTrabW <victoria.tw@hotmail.com>
2023-03-31 22:15:18 +0200

    change

--- scr/training_model.py
+++ scr/training_model.py
@@ -99,6 +99,9 @@ for filename in os.listdir(data_dir):
         comments_df = pd.read_csv(data_dir + "/" + filename)
         all_comments.extend(list(comments_df["commentBody"].values))
 
+#clean up the data
+all_comments = [h for h in all_comments if h != "Unknown"]
+corpus = [clean_text(x) for x in all_comments]
 
 
 
